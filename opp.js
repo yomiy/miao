b3bd3d86fdea6de54f136e5073a5dec86e2bc712
hao <hao@qq.com>
2023-05-30 22:21:49 +0800

    e

--- opp.js
+++ opp.js
@@ -1,6 +1,5 @@
-
   class Vector{
-    Vector(x,y){
+    constructor(x,y){
       this.x = x
       this.y = y
     }
@@ -14,37 +13,91 @@
       var y = this.y - vector.y
       return new Vector(x, y)
     }
-    get function() {
+    get length() {
       return Math.sqrt(this.x * this.x + this.y * this.y)
     }
   }
 
   class Complex{
-    Cpmplex(real, imag){
+    constructor(real, imag){
       this.real = real
       this.imag = imag
     }
-    plus(complex){
+    plus(c){
       var real = this.real + c.real
       var imag = this.imag + c.imag
       return new Complex(real, imag)
     }
-    minus(complex){
+    minus(c){
       var real = this.real - c.real
       var imag = this.imag - c.imag
       return new Complex(real, imag)
     }
-    mul(complex){
+    mul(c){
       var real = this.real * c.real - this.imag * c.imag
       var imag = this.real * c.imag + this.imag * c.real
       return new Complex(real, imag)
     }
-    div(complex){
+    div(c){
       var helper = new Complex(c.real, -c.imag)
       var up = this.mul(helper)
       var down = c.mul(helper) 
       var real = up.real / down.real
       var imag = up.imag / down.real
       return new Complex(real, imag)
+    }
+  }
+
+  class LinkedList{
+    constructor(){
+      this.head = null
+      this.tail = null
+    }
+    append(val){
+      var node = {
+        val: val, 
+        next: null
+      }
+      if (this.head == null) {
+        this.head = this.tail = node
+        return
+      } else {
+        this.tail.next = node
+        this.tail = node
+        return
+      }
+    }
+    prepend(val) {
+      var node = {
+        val: val,
+        next: null
+      }
+      if (this.head == null) {
+        this.head = this.tail = node
+        return
+      } else {
+        node.next = this.head
+        this.head = node
+        return
+      }
+    }
+    at(idx) {
+      if (this.head === this.tail === null) return null
+      var p = this.head
+      var count = 0
+      while (count < idx) {
+        p = p.next
+        count++
+      }
+      return p.val
+    }
+    get size () {
+      var p = this.head
+      var l = 0
+      while (p) {
+        l++
+        p = p.next
+      }
+      return l
     }
   }
